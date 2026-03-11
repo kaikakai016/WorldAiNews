c3e000ff1531edd1b609da0ff3082e3887697373
kai <himerrakovou@gmail.com>
2026-03-11 19:13:31 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,21 @@
 # config.py
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 
 class Config:
+    # Telegram
     TELEGRAM_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
     CHANNEL_ID = os.getenv('TELEGRAM_CHANNEL_ID')
-    POST_INTERVAL_HOURS = int(os.getenv('POST_INTERVAL_HOURS', '3'))
     
     @staticmethod
     def get_current_time():
-        return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        return datetime.now().strftime("%H:%M:%S")
+    
+    @staticmethod
+    def get_next_post_time():
+        """Время следующего поста (+5 минут)"""
+        next_time = datetime.now() + timedelta(minutes=5)
+        return next_time.strftime("%H:%M:%S")
     
     @staticmethod
     def validate():
